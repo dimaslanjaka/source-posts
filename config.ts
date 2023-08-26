4257b8491ee81d9890f9b5d720493d09f66be748
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-08-26 07:41:07 +0000

    fix: reference `__dirname` undefined

--- config.ts
+++ config.ts
@@ -1,4 +1,8 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const postsDir = path.join(__dirname, 'posts');
 export const sourcePostsRoot = __dirname;
