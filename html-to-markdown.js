9e659129abc86c8dc336dbf5797ec47fbc8f5093
dimaslanjaka <dimaslanjaka@gmail.com>
2023-11-18 08:00:55 +0700

    add html body to markdown converter

---
+++ html-to-markdown.js
@@ -0,0 +1,50 @@
+const hpp = require('hexo-post-parser')
+const TurndownService = require('turndown')
+const glob = require('glob')
+const Promise = require('bluebird')
+const { fs, path, writefile } = require('sbg-utility')
+const yaml = require('yaml')
+
+const files = Promise.resolve(
+  glob.glob('posts/**/*.md', { ignore: ['**/node_modules/**', '**/readme*'] })
+)
+
+files.then((files) => {
+  console.log('total', files.length)
+  return files
+})
+
+files.each((file) => {
+  try {
+    file = path.resolve(file)
+    const result = hpp.parsePostFM(fs.readFileSync(file, 'utf-8'))
+    if (!result) return console.error('fail parse', file)
+    if (!result.body) return console.error('fail render', file)
+    if (!/<div/gm.test(result.body))
+      // return console.error(file, 'not html body')
+      return
+    if (/codepen/m.test(result.body))
+      return console.error(file, 'contains codepen')
+    if (new RegExp('<style', 'm').test(result.body))
+      return console.error(file, 'contains style')
+    if (new RegExp('<script', 'm').test(result.body))
+      return console.error(file, 'contains script')
+    if (new RegExp('<pre', 'm').test(result.body))
+      return console.error(file, 'contains pre')
+    console.log('convert', file)
+    const turndownService = new TurndownService()
+    turndownService.keep(['script', 'ins', 'iframe', 'style'])
+    const markdown = turndownService.turndown(result.body)
+    const metadata = yaml.stringify(result.attributes)
+    const post = `
+---
+${metadata}
+---
+
+${markdown}
+    `.trim()
+    writefile(file, post)
+  } catch {
+    console.log('fail parse', file)
+  }
+})
