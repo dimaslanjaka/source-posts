58b9b992131b237a24b311858168f2582e0b1f2c
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-18 19:56:01 +0700

    feat: https with socks proxy test

---
+++ test/proxy/https-socks.ts
@@ -0,0 +1,31 @@
+import Bluebird from 'bluebird';
+import https from 'https';
+import { SocksProxyAgent } from 'socks-proxy-agent';
+import { readProxy } from '../../src/utils/proxy';
+
+// const agent = new SocksProxyAgent('socks://username:password@45.192.223.93:33544');
+
+(async () => {
+  const proxies = readProxy();
+
+  while (proxies.length > 0) {
+    const proxy = proxies.shift();
+    if (!proxy) continue;
+    const agent = new SocksProxyAgent('socks://' + proxy);
+
+    await new Bluebird((resolve) => {
+      https
+        .get('https://ip.me', { agent }, (res) => {
+          console.log(proxy, res.statusCode);
+          resolve();
+        })
+        .on('error', (e) => {
+          console.log(proxy, e.message);
+          resolve();
+        })
+        .on('end', function () {
+          resolve();
+        });
+    });
+  }
+})();
