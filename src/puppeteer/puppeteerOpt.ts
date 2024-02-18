57420c9f2fb02afd10680556363726d56aece4ff
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-18 17:39:59 +0700

    fix: missing return

--- src/puppeteer/puppeteerOpt.ts
+++ src/puppeteer/puppeteerOpt.ts
@@ -1,10 +1,11 @@
-import { join } from 'path';
-import { PuppeteerLaunchOptions } from 'puppeteer';
+import { deepmerge } from 'deepmerge-ts';
+import * as puppeteer from 'puppeteer';
+import { path } from 'sbg-utility';
 
-export const puppeteerOpt: PuppeteerLaunchOptions = {
+export const puppeteerOpt: puppeteer.PuppeteerLaunchOptions = {
   headless: false,
   args: [
-    '--user-data-dir=' + join(process.cwd(), 'tmp/puppeteer_profile'),
+    '--user-data-dir=' + path.join(process.cwd(), 'tmp/puppeteer_profile'),
     '--no-sandbox',
     '--disable-setuid-sandbox',
     '--disable-infobars',
@@ -16,3 +17,25 @@ export const puppeteerOpt: PuppeteerLaunchOptions = {
     '--user-agent="Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z‡ Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"'
   ]
 };
+
+interface MyPuppeteerOptions extends puppeteer.PuppeteerLaunchOptions {
+  proxy?: string;
+}
+
+export default function getPuppeteerOptions(options: MyPuppeteerOptions = {}) {
+  let launchOpt = deepmerge(puppeteerOpt, options);
+  if (launchOpt.proxy) {
+    const proxyAddress = launchOpt.proxy;
+    launchOpt.ignoreHTTPSErrors = true;
+    launchOpt = deepmerge(launchOpt, {
+      args: [
+        `--proxy-server=${proxyAddress}`,
+        `--ignore-certificate-errors`,
+        `--no-sandbox`,
+        `--disable-setuid-sandbox`
+      ]
+    });
+    delete launchOpt.proxy;
+  }
+  return launchOpt;
+}
