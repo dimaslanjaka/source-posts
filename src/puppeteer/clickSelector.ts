80501dc9b8371a850a00a2d0611677a0e77c64f0
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-19 07:40:12 +0700

    return boolean

--- src/puppeteer/clickSelector.ts
+++ src/puppeteer/clickSelector.ts
@@ -3,7 +3,9 @@ import { Page } from 'puppeteer';
 export default async function clickSelector(page: Page, selector: string) {
   if ((await page.$(selector)) !== null) {
     await page.click(selector).catch((e) => console.log('failed click', selector, e.message));
+    return true;
   } else {
-    console.log('no element found', selector);
+    console.error('no element found', selector);
+    return false;
   }
 }
